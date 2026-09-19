ad2c02200a06952d358e8c517c00f76367b36c65
Tajudeen WWariz <thabidemi@gmail.com>
2026-09-19 09:09:33 +0100

    fix: features v2

--- src/wallet/wallet.module.ts
+++ src/wallet/wallet.module.ts
@@ -1,1 +1,18 @@
-import { Module } from '@nestjs/common';import { TypeOrmModule } from '@nestjs/typeorm';import { AuthModule } from '../auth/auth.module';import { PatientWallet } from './entities/patient-wallet.entity';import { PatientWalletEntry } from './entities/patient-wallet-entry.entity';import { PatientWalletController } from './patient-wallet.controller';import { PatientWalletService } from './patient-wallet.service';@Module({imports:[AuthModule,TypeOrmModule.forFeature([PatientWallet,PatientWalletEntry])],controllers:[PatientWalletController],providers:[PatientWalletService],exports:[PatientWalletService]})export class WalletModule{}
+import { Module } from "@nestjs/common";
+import { TypeOrmModule } from "@nestjs/typeorm";
+import { AuthModule } from "../auth/auth.module";
+import { PatientWallet } from "./entities/patient-wallet.entity";
+import { PatientWalletEntry } from "./entities/patient-wallet-entry.entity";
+import { PatientWalletController } from "./patient-wallet.controller";
+import { PatientWalletService } from "./patient-wallet.service";
+import { User } from "src/users/entities/user.entity";
+@Module({
+  imports: [
+    AuthModule,
+    TypeOrmModule.forFeature([PatientWallet, PatientWalletEntry, User]),
+  ],
+  controllers: [PatientWalletController],
+  providers: [PatientWalletService],
+  exports: [PatientWalletService],
+})
+export class WalletModule {}
